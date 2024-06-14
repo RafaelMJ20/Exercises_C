3d791e5f57576ad03056feba53146f63af5de29d
Rafa <mirandarafa010@gmail.com>
2024-06-14 12:00:00 -0600

    Añadir ejercicio_5.c en 2024-06-14T12:00:00

---
+++ ejercicio_5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+main ()
+{
+  int a, b;
+  char respuesta;
+
+do
+ {
+  printf ("Introduce el valor del primer número:");
+  scanf ("%d", &a);
+  printf ("\n");
+  printf ("Introduce el valor del segundo número:");
+  scanf ("%d", &b);
+  printf ("\n");
+
+  if (a==b)
+    {
+      printf ("El primer número, %d y el segundo, %d, son iguales \n", a, b);
+    }
+  else if (a>b)
+         {
+           printf ("El primer número, %d es mayor que el segundo, %d \n", a, b);
+         }
+       else
+         {
+           printf ("El segundo número, %d es mayor que el primero, %d \n", b, a);
+         }
+  printf ("¿Desea ejecutarlo otra vez? (Pulse s o S. Cualquier otra tecla para terminar)");
+  scanf ("%c", respuesta);
+
+  printf ("\n");
+ }while (respuesta=='s' || respuesta=='S');
+
+}
