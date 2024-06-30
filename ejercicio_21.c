2dd432222c27962cae34672d9c9bc52c487ef5b1
Rafa <mirandarafa010@gmail.com>
2024-06-30 12:00:00 -0600

    Añadir ejercicio_21.c en 2024-06-30T12:00:00

---
+++ ejercicio_21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+main ()
+{
+  int i,j;
+  
+  for (i=1;i<20;i++)
+    {
+      printf ("\n");
+      for(j=i;j<20;j++)
+        printf("*");
+    }
+
+  for (i=20;i>0;i--)
+    {
+      printf ("\n");
+      for(j=i;j<20;j++)
+        printf("*");
+    }
+printf("\n");
+}
