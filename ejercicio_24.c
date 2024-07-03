18d859d4aa26a7f61046b1320739eb21dc6f4504
Rafa <mirandarafa010@gmail.com>
2024-07-03 12:00:00 -0600

    Añadir ejercicio_24.c en 2024-07-03T12:00:00

---
+++ ejercicio_24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+main ()
+{
+  int n;
+ 
+  printf ("Introduce un valor: ");
+  scanf ("%d", &n);
+ 
+  printf ("%d \t %d \t %d \t %d \t %d \t %d \n", n,n*n,n*n*n,n*n*n*n,n*n*n*n*n,n*n*n*n*n*n);
+	
+}
